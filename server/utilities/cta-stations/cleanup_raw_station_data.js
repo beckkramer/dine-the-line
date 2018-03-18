2623f3e0023250cf11e340bdca727a861a97f1dc
beckkramer <kramer.beck@gmail.com>
2018-03-18 00:02:05 -0500

    Updates to station utility to combine lines properly.

--- server/utilities/cta-stations/cleanup_raw_station_data.js
+++ server/utilities/cta-stations/cleanup_raw_station_data.js
@@ -22,49 +22,100 @@ const newFormat = {
   map_id: 0,
 };
 
+const consolidateStations = (rawStationData) => {
+  const stations = {};
+
+  rawStationData.forEach(station => {
+    if (!stations[station.station_descriptive_name]) {
+      stations[station.station_descriptive_name] = {};
+    }
+  });
+
+  return stations;
+};
+
+const createActiveLineArray = (stationLines) => {
+  
+  let activeLines = [];
+  
+  Object.entries(stationLines).forEach(line => {
+    if(line[1] === true) {
+      activeLines.push(line[0]);
+    }
+  });
+
+  return activeLines;
+}
+
+formatStation = (rawStation) => {
+
+  const combinedPurple = rawStation.p || rawStation.pexp;
+
+  const thisStation = {
+    accessible: rawStation.ada,
+    name : rawStation.station_name,
+    lines : {
+      all: {
+        blue : rawStation.blue,
+        brown : rawStation.brn,
+        green : rawStation.g,
+        orange : rawStation.o,
+        pink : rawStation.pnk,
+        purple : combinedPurple,
+        red : rawStation.red,
+        yellow : rawStation.y,
+      },
+      active: []
+    }, 
+    location : rawStation.location.coordinates.reverse().join(','),
+    stop_id: rawStation.stop_id,
+    map_id: rawStation.map_id,
+    station_descriptive_name: rawStation.station_descriptive_name
+  };
+
+  return thisStation;
+}
+
+// If any instance of a station has a specific line, set that line to true
+// in the combined station line data.
+const overrideLineValue = (stationLines, allStationLines) => {
+
+  Object.keys(stationLines).forEach(line => {
+    if (stationLines[line]) {
+      allStationLines[line] = true;
+    }
+  });
+
+  return allStationLines;
+}
+
 fetch('./raw_station_data.json').then(response => {
 
   response.json().then(result => {
 
-    result.forEach(station => {
+    const consolidatedStations = consolidateStations(result);
 
-      // Check if this station's descriptive name is already in cleanStation array
-      // to prevent both directions of travel from being added.
-      if (!cleanStationNames.includes(station.station_descriptive_name)) {
+    result.forEach((station, index, []) => {
 
-        let thisStation = {
-          accessible: station.ada,
-          name : station.station_name,
-          lines : {
-            all: {
-              blue : station.blue,
-              brown : station.brn,
-              green : station.g,
-              orange : station.o,
-              pink : station.pnk,
-              purple : station.p,
-              red : station.red,
-              yellow : station.y,
-            },
-            active: []
-          }, 
-          location : station.location.coordinates.reverse().join(','),
-          stop_id: station.stop_id,
-          map_id: station.map_id,
-        };
+      let thisStation = formatStation(station);
 
-        let formattedLines = [];
+      if (station.station_descriptive_name === thisStation.station_descriptive_name) {
 
-        Object.entries(thisStation.lines.all).forEach(line => {
-          if(line[1] === true) {
-            thisStation.lines.active.push(line[0]);
-          }
-        });
-
-        cleanStationNames.push(station.station_descriptive_name);
-        cleanStations.push(thisStation);
+        // Check if consolidated stations has an entry for this station already
+        if (!consolidatedStations[station.station_descriptive_name].station_descriptive_name) {
+          consolidatedStations[station.station_descriptive_name] = thisStation;
+        
+          // If the entry exists, update any `true` line values for this station to combine inner/outer track values, etc.
+        } else {
+          const currentLineValues = consolidatedStations[station.station_descriptive_name].lines.all;
+          consolidatedStations[station.station_descriptive_name].lines.all = overrideLineValue(thisStation.lines.all, currentLineValues);
+        }
       }
+    });
 
+    Object.values(consolidatedStations).forEach(station => {
+      station.lines.active = createActiveLineArray(station.lines.all);
+      cleanStations.push(station);
     });
 
     document.getElementById('main').innerHTML = JSON.stringify(cleanStations);
