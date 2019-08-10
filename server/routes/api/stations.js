165894d33adada0ed71f55164cd196821e8fadc2
beckkramer <kramer.beck@gmail.com>
2019-08-10 12:16:52 -0500

    Updates server to have per-station api calls

--- server/routes/api/stations.js
+++ server/routes/api/stations.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const stations = require('../data/cta_stations.json');
 
 /* GET stations listing. */
-router.get('/', function(request, response, next) {
+router.get('/all', function(request, response, next) {
   response.status(200).json(stations);
+});
+
+router.get('/:id', function(request, response, next) {
+  const station = stations.find(station => {
+    return station.stop_id === request.params.id
+  })
+  response.status(200).json(station);
 });
 
 module.exports = router;
